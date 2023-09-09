2ec7b5f668abdf668e0d8e9d38729db5f5ec193a
wilmar <wilmarcastro46@gmail.com>
2023-09-09 01:05:02 -0500

    Add pets functional

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import { ApolloServer,gql } from "apollo-server";
-import axios from "axios";
+import { ApolloServer,gql } from "apollo-server"
+import axios from "axios"
 
 const typeDefs = gql`
     type Pet{
         _id: String!
         name: String!
-        image: String!
+        image: String
         race: String!
         age: String!
         size: String!
         appointments: [String!]
         createdAt: String!
-        updateAt: String!
+        updatedAt: String!
     }
 
     type Appointment{
@@ -21,12 +21,23 @@ const typeDefs = gql`
         type: String!
         pet: String!
         createdAt: String!
-        updateAt: String!
+        updatedAt: String!
     }
 
     type Query{
         allPets: [Pet]
         allAppointments: [Appointment]
+        petByName(name: String!): Pet
+    }
+
+    type Mutation{
+        registPet(
+            name: String!
+            image: String
+            race: String!
+            age: String!
+            size: String!
+        ): Pet
     }
 `
 
@@ -41,6 +52,29 @@ const resolvers = {
             const {data: Appointments} = await axios.get('https://petbackend.vercel.app/appointment')
 
             return Appointments
+        },
+        petByName: async (parent,args) => {
+            const {data: Pets} = await axios.get('https://petbackend.vercel.app/pet')
+            const pet = Pets.find(p => p.name === args.name)
+
+            return pet
+        }
+    },
+    Mutation: {
+        registPet: async (parent, args) => {
+            try{
+                const pet = await axios.post('https://petbackend.vercel.app/pet', {
+                    name: args.name,
+                    image: args.image,
+                    race: args.race,
+                    age: args.age,
+                    size: args.size
+                })
+                const p = {...args}
+                return p
+            } catch (error) {
+                throw new Error("No se pudo agregar")
+            }
         }
     }
 }
